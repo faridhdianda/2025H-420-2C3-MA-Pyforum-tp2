c605a4b17c98de9876a3f3da5262ce104e8347eb
Dianda Dianda <e6323808@cmaisonneuve.qc.ca>
2025-04-23 11:54:21 -0400

    mis a jour sur le projet

--- src/pyforum/utilisateur.py
+++ src/pyforum/utilisateur.py
@@ -1,3 +1,6 @@
+ 
+import csv
+import ast
 
 class Utilisateur:
     def __init__(self, identifiant, nom_utilisateur, courriel, mot_de_passe, forums_inscrits=None):
@@ -7,10 +10,6 @@ class Utilisateur:
         self.mot_de_passe = mot_de_passe
         self.forums_inscrits = forums_inscrits if forums_inscrits is not None else []
 
-    def inscrire_forum(self, forum_id):
-        """Alias pour joindre_forum"""
-        self.joindre_forum(forum_id)
-
     def joindre_forum(self, forum_id):
         if forum_id not in self.forums_inscrits:
             self.forums_inscrits.append(forum_id)
@@ -19,7 +18,7 @@ class Utilisateur:
         return (f"Utilisateur(id={self.identifiant}, nom_utilisateur='{self.nom_utilisateur}', "
                 f"courriel='{self.courriel}', forums_inscrits={self.forums_inscrits})")
 
-    def to_dict(self):
+    def to_user(self):
         return {
             "identifiant": self.identifiant,
             "nom_utilisateur": self.nom_utilisateur,
@@ -29,7 +28,7 @@ class Utilisateur:
         }
 
     @classmethod
-    def from_dict(cls, data):
+    def from_user(cls, data):
         return cls(
             identifiant=data["identifiant"],
             nom_utilisateur=data["nom_utilisateur"],
@@ -37,3 +36,19 @@ class Utilisateur:
             mot_de_passe=data["mot_de_passe"],
             forums_inscrits=data.get("forums_inscrits", [])
         )
+
+    @staticmethod
+    def charger_depuis_csv(utilisateur):
+        utilisateurs = []
+        with open("src/pyforum/data/user.csv",mode="r" , newline='', encoding='utf-8') as csvfile:
+            lecteur = csv.DictReader(csvfile)
+            for ligne in lecteur:
+                utilisateur = Utilisateur(
+                    identifiant=int(ligne["identifiant"]),
+                    nom_utilisateur=ligne["nom_utilisateur"],
+                    courriel=ligne["courriel"],
+                    mot_de_passe=ligne["mot_de_passe"],
+                    forums_inscrits=ast.literal_eval(ligne["forums_inscrits"])  # convertit "[100, 101]" → [100, 101]
+                )
+                utilisateurs.append(utilisateur)
+        return utilisateurs
